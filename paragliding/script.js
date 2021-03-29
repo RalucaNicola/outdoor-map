d74b70a0b120c358207c25e19bbe3ea21f0a0f4c
Raluca Nicola <RNicola@esri.com>
2021-03-29 18:46:40 +0200

    Add copyright and option to turn wall viz off

--- paragliding/script.js
+++ paragliding/script.js
@@ -5,8 +5,6 @@ import SceneView from "esri/views/SceneView.js";
 import GeoJSONLayer from "esri/layers/GeoJSONLayer.js";
 import ElevationProfile from "esri/widgets/ElevationProfile.js";
 import ElevationProfileLineGround from "esri/widgets/ElevationProfile/ElevationProfileLineGround.js";
-
-import { whenNotOnce } from "esri/core/watchUtils.js";
 import { SimpleRenderer } from "esri/renderers.js";
 import { LineSymbol3D, LineSymbol3DLayer, PathSymbol3DLayer } from "esri/symbols.js";
 import ElevationProfileLineInput from "esri/widgets/ElevationProfile/ElevationProfileLineInput.js";
@@ -38,10 +36,16 @@ const view = new SceneView({
   },
 });
 
+view.ui.add("sideMenu", "top-right");
+
 const epw = new ElevationProfile({
   view,
+  container: "elevationProfileContainer",
   profiles: [
-    new ElevationProfileLineGround({ color: [159, 207, 130] }),
+    new ElevationProfileLineGround({
+      color: [159, 207, 130],
+      visible: false
+    }),
     new ElevationProfileLineInput({ color: [245, 203, 66] }),
   ],
   visibleElements: {
@@ -50,8 +54,6 @@ const epw = new ElevationProfile({
     settingsButton: false,
   },
 });
-
-view.ui.add(epw, "top-right");
 
 view.when(async () => {
   const response = await fetch("./paragliding.gpx");
@@ -65,7 +67,27 @@ view.when(async () => {
 
   const geojsonLayer = new GeoJSONLayer({
     url: geojsonurl,
-    renderer: new SimpleRenderer({
+    copyright: "Paragliding track provided by Remo Hosig",
+    renderer: getRenderer({wallProjection: false}),
+    elevationInfo: {
+      mode: "absolute-height",
+    },
+  });
+
+  map.add(geojsonLayer);
+
+  await geojsonLayer.load();
+
+  document.getElementById("enableWallProjection").addEventListener("click", (event) => {
+    geojsonLayer.renderer = getRenderer({wallProjection: event.target.checked});
+  });
+
+  addToElevationProfile(geojsonLayer);
+});
+
+function getRenderer(options) {
+  if (options.wallProjection) {
+    return new SimpleRenderer({
       symbol: new LineSymbol3D({
         symbolLayers: [
           new LineSymbol3DLayer({
@@ -81,23 +103,25 @@ view.when(async () => {
             cap: "butt",
             anchor: "top",
             profileRotation: "heading",
-          }),
-        ],
+          })
+        ]
+      })
+    })
+  } else {
+    return new SimpleRenderer({
+      symbol: new LineSymbol3D({
+        symbolLayers: [
+          new LineSymbol3DLayer({
+            material: { color: [245, 203, 66] },
+            size: "3px",
+          })
+        ]
       }),
-    }),
-    elevationInfo: {
-      mode: "absolute-height",
-    },
-  });
+    })
+  }
+}
 
-  map.add(geojsonLayer);
-
-  await geojsonLayer.load();
-
-  selectLayer(geojsonLayer);
-});
-
-async function selectLayer(layer) {
+async function addToElevationProfile(layer) {
 
   const { features } = await layer.queryFeatures();
 
